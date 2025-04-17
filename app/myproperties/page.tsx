f548db212742b7c27f4d5a9c79d144c056bc7557
urutear <keitadance0208@gmail.com>
2025-04-18 05:42:43 +0900

    add reservation and landlord pages with routes

---
+++ app/myproperties/page.tsx
@@ -0,0 +1,9 @@
+import styles from './myproperties.module.css';
+
+export default function MyPropertiesPage() {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.h1}> My properties</h1>
+    </main>
+  )
+}
