8921f0b1614efbd2d8b5a71d29fe51af66332a17
urutear <keitadance0208@gmail.com>
2025-04-18 15:43:26 +0900

    add inbox and messages page

--- app/components/navbar/UserNav.tsx
+++ app/components/navbar/UserNav.tsx
@@ -1,6 +1,10 @@
+'use client'
+
+import { useState } from 'react';
 import styles from '@/app/components/navbar/navbar.module.css';
 
 export default function UserNav() {
+  const [isOpen, setIsOpen] = useState(true);
   return (
     <div className={styles.usermenu}>
       <button className={styles.usermenu_button}>
@@ -12,8 +16,13 @@ export default function UserNav() {
         <svg fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className={styles.icon}>
           <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
         </svg>
-
       </button>
+
+      {isOpen && (
+        <div className={styles.popup}>
+          asdfe
+        </div>
+      )}
     </div>
   )
 }
